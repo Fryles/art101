efcf8bdea156a36d9b6f4d397bfacded399d59a9
fryles <mylesmarr@gmail.com>
2023-10-22 21:50:14 -0700

    lab 4

--- lab4/js/lab.js
+++ lab4/js/lab.js
@@ -1,21 +1,14 @@
-// index.js - purpose and description here
-// Author: Your Name
-// Date:
+// Rock Paper Scissors Psuedocode
+// Myles Marr <mmarr@ucsc.edu>
+// October 22, 2023
 
-// Constants
-
-// Functions
-
-// this is an example function and this comment tells what it doees and what parameters are passed to it.
-function myFunction(param1, param2) {
-  // some code here
-  // return results;
-}
-
-function main() {
-  console.log("Main function started.");
-  // the code that makes everything happen
-}
-
-// let's get this party started
-main();
+// Have each player their three choices (rock, paper, or scissors)
+  // Player 1 selects their input from the 3 choices
+  // Player 2 selects their input from the 3 choices
+// Both inputs are compared, and a winner is chosen
+// The winning player’s score increases by 1
+  // Retrive winning player’s score
+  // Add 1 to winning player’s score
+// The updated score count for both players is shown
+// Game resets
+  // (Restarts from the beginning)
