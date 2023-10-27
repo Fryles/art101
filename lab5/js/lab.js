5a4b1cb045e3a2b4b1cc72da1a218a9e55592816
fryles <mylesmarr@gmail.com>
2023-10-27 06:54:37 -0700

    lab5

--- lab5/js/lab.js
+++ lab5/js/lab.js
@@ -1,21 +1,18 @@
-// index.js - purpose and description here
-// Author: Your Name
-// Date:
+// lab.js - js file for lab 5
+// Myles Marr <mmarr@ucsc.edu>
+// October 25, 2023
 
-// Constants
 
-// Functions
+var make = "Honda";
+var model = "Element";
+var color = "Deep Pearl Black";
+var year = 2011;
+var ownIt = true;
+var age = new Date().getFullYear() - year;
 
-// this is an example function and this comment tells what it doees and what parameters are passed to it.
-function myFunction(param1, param2) {
-  // some code here
-  // return results;
-}
-
-function main() {
-  console.log("Main function started.");
-  // the code that makes everything happen
-}
-
-// let's get this party started
-main();
+document.writeln("Make: " + make + "<br>");
+document.writeln("Model: " + model + "<br>");
+document.writeln("Color: " + color + "<br>");
+document.writeln("Year: " + year + "<br>");
+document.writeln("Age: " + age + " years <br>");
+document.writeln("Own it: " + ownIt + "<br>");
